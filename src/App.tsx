46fb143bef9ea489ed824faa2dd013ce3fe9adee
Kensaku Tanaka <tannakaken@gmail.com>
2025-08-06 00:31:35 +0900

    ビルドできるように修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { VFXProvider, VFX } from 'react-vfx'
+import { VFXProvider, VFXDiv } from 'react-vfx'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -10,37 +10,35 @@ function App() {
   return (
     <VFXProvider>
       <div className="app-container">
-        <VFX>
-          <div>
-            <a href="https://vitejs.dev" target="_blank">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
-        </VFX>
+        <VFXDiv>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </VFXDiv>
         
-        <VFX shader="glitch">
+        <VFXDiv shader="glitch">
           <h1>React + Vite</h1>
-        </VFX>
+        </VFXDiv>
         
         <div className="card">
-          <VFX shader="pixelate">
+          <VFXDiv shader="pixelate">
             <button onClick={() => setCount((count) => count + 1)}>
               count is {count}
             </button>
-          </VFX>
+          </VFXDiv>
           <p>
             Edit <code>src/App.jsx</code> and save to test HMR
           </p>
         </div>
         
-        <VFX shader="duotone" uniforms={{ color1: [1, 0, 1], color2: [0, 1, 1] }}>
+        <VFXDiv shader="duotone" uniforms={{ color1: [1, 0, 1], color2: [0, 1, 1] }}>
           <p className="read-the-docs">
             Click on the React and Vite logos to learn more
           </p>
-        </VFX>
+        </VFXDiv>
       </div>
     </VFXProvider>
   )
